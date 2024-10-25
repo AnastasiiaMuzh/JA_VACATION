bb28682e3fae0775e0a2840a998b59c178402502
AnastasiiaMuzh <anastasiia.muzhzhavlev@outlook.com>
2024-10-24 20:45:03 -0600

    Refactoring 'Delete review image'

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -29,65 +29,6 @@ const validateSignup = [
         .withMessage('Password must be 6 characters or more.'),
     handleValidationErrors
 ];
-//validation for signup
-// const validateSignup = [
-//     check('email')
-//         .exists({ checkFalsy: true })
-//         .isEmail()
-//         .withMessage('Invalid email'),
-//     check('username')
-//         .exists({ checkFalsy: true })
-//         .isLength({ min: 4 })
-//         .withMessage('Username is required'),
-//     check('username')
-//         .not()
-//         .isEmail()
-//         .withMessage('Username cannot be an email.'),
-//     check('password')
-//         .exists({ checkFalsy: true })
-//         .isLength({ min: 6 })
-//         .withMessage('Password must be 6 characters or more.'),
-//     check('firstName')
-//         .exists({ checkFalsy: true })
-//         .isLength({ min: 4 })
-//         .withMessage('First Name is required'),
-//     check('lastName')
-//         .exists({ checkFalsy: true })
-//         .isLength({ min: 4 })
-//         .withMessage('Last Name is required'),
-//     handleValidationErrors
-// ];
-
-// Sign up
-// router.post("/", validateSignup, async (req, res) => {
-//     const { firstName, lastName, email, password, username } = req.body;
-//     const hashedPassword = bcrypt.hashSync(password);
-//     const user = await User.create({ firstName, lastName, email, username, hashedPassword });
-
-//     if (email || username) {
-//         return res.status(500).json({
-//             "message": "User already exists",
-//             "errors": {
-//                 "email": "User with that email already exists",
-//                 "username": "User with that username already exists"
-//             }
-//         })
-//     }
-
-//     const safeUser = {
-//         id: user.id,
-//         firstName: user.firstName,
-//         lastName: user.lastName,
-//         email: user.email,
-//         username: user.username,
-//     };
-
-//     await setTokenCookie(res, safeUser);
-
-//     return res.status(201).json({
-//         user: safeUser,
-//     });
-// });
 
 // Sign up
 router.post("/", validateSignup, async (req, res) => {

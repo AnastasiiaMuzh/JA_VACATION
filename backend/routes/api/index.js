be271e1ffda72d90d5789018f314975f73a653c5
Anastasiia Muzhzhavlev <ana@Anastasiias-MacBook-Pro.local>
2024-11-16 17:33:23 -0700

    somthing change

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,11 +1,12 @@
 // backend/routes/api/index.js
 const router = require('express').Router();
 const { setTokenCookie, requireAuth } = require('../../utils/auth.js');
-const { User, Spot, Review, SpotImage, ReviewImage } = require('../../db/models');
+const { User, Spot, Review, SpotImage, ReviewImage, Booking } = require('../../db/models');
 const sessionRouter = require('./session.js');
 const usersRouter = require('./users.js');
 const spotsRouter = require('./spots.js');
 const reviewRouter = require('./reviews.js');
+const bookingRouter = require('./bookings.js');
 
 
 // GET /api/set-token-cookie
@@ -111,6 +112,7 @@ router.use('/session', sessionRouter);
 router.use('/users', usersRouter);
 router.use('/spots', spotsRouter);
 router.use('/reviews', reviewRouter);
+router.use('/bookings', bookingRouter);
 
 router.post('/test', function (req, res) {
   res.json({ requestBody: req.body });

10f4315a7b36f73434650c370b4e1951d409e9ec
AnastasiiaMuzh <anastasiia.muzhzhavlev@outlook.com>
2024-12-16 23:29:07 -0700

    Fix the password issue

--- frontend/src/components/SignupFormModal/SignupFormModal.jsx
+++ frontend/src/components/SignupFormModal/SignupFormModal.jsx
@@ -90,11 +90,11 @@ function SignupFormModal() {
     <div className="signup-form-container">
       <h1>Sign Up</h1>
       <form onSubmit={handleSubmit} noValidate>
-        {["firstName", "lastName", "email", "username",  "password", "confirm Password"].map((field) => (
+        {["firstName", "lastName", "email", "username",  "password", "confirmPassword"].map((field) => (
           <label key={field}>
             {field[0].toUpperCase() + field.slice(1).replace("Name", " Name")}
             <input
-              type={field.includes("password") || field.includes("confirm Password") ? "password" : "text"}
+              type={field.includes("password") || field.includes("confirmPassword") ? "password" : "text"}
               name={field}
               value={formData[field]}
               onChange={handleChange}
